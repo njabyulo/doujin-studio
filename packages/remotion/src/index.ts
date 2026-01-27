970ed93cae9d90a0ef59a0827de22806dd86e2ef
Njabulo Majozi <services@njabulomajozi.com>
2026-01-27 19:25:25 +0200

    feat(remotion): export Remotion components
    - Export RemotionRoot, Master, and Scene components
    - Make components available for use in other packages

--- packages/remotion/src/index.ts
+++ packages/remotion/src/index.ts
@@ -1,2 +1,8 @@
 // Export types and schemas
 export * from './types';
+
+// Export Remotion components
+export { Master } from './Master';
+export { RemotionRoot } from './Root';
+export { Scene } from './Scene';
+
