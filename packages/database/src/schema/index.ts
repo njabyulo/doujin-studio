0dd48586e26155a3b6a536d51b2ec36e0d8f0693
Njabulo Majozi <services@njabulomajozi.com>
2026-02-10 08:42:12 +0200

    chore(db): remove unused schema exports and columns

--- packages/database/src/schema/index.ts
+++ packages/database/src/schema/index.ts
@@ -1,11 +1,6 @@
-
 export * from "./asset";
 export * from "./auth";
-export * from "./checkpoint";
-export * from "./idempotency_key";
-export * from "./message";
 export * from "./project";
 export * from "./project_member";
-export * from "./render_job";
 export * from "./timeline";
 export * from "./timeline_version";
